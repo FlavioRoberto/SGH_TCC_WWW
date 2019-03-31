46b3e778c7380ef1255f1721d0cff81e2903221c
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2019-03-31 19:11:33 -0300

    Adição dos campos no formulário turno

--- www/src/app/main/gerenciamento/turno/turno.module.ts
+++ www/src/app/main/gerenciamento/turno/turno.module.ts
@@ -3,7 +3,13 @@ import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { FuseSharedModule } from '@fuse/shared.module';
 import { TurnoComponent } from './turno.component';
-import { MatIconModule, MatTableModule } from '@angular/material';
+import {
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatFormFieldControl,
+    MatInputModule
+} from '@angular/material';
 import { HeaderDescricaoModule } from 'app/layout/components/header-descricao/header-descricao.module';
 import { DataBarModule } from 'app/layout/components/databar/data-bar.module';
 
@@ -21,8 +27,9 @@ const routes = [
         TranslateModule,
         FuseSharedModule,
         MatIconModule,
-        HeaderDescricaoModule,
         MatTableModule,
+        MatInputModule,
+        HeaderDescricaoModule,
         DataBarModule
     ],
     exports: [TurnoComponent]
