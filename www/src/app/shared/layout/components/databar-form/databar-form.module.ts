54d3697a739a85090fd6cc823700a55914384b8e
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2019-12-24 18:58:43 -0300

    Remoção de código desnecessário

--- www/src/app/shared/layout/components/databar-form/databar-form.module.ts
+++ www/src/app/shared/layout/components/databar-form/databar-form.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { DataBarFormComponent } from './databar-form.component';
 import { CompartilhadoModule } from '@compartilhado/compartilhado.module';
 import { MaterialCoreModule } from '@compartilhado/material-core/material-core.module';
-import { DataBarModule } from '@breaking_dev/ic-databar-lib';
+import { DataBarModule, ConfirmaDialogComponent } from '@breaking_dev/ic-databar-lib';
 
 @NgModule({
     declarations: [DataBarFormComponent],
     imports: [DataBarModule, CompartilhadoModule, MaterialCoreModule],
-    exports: [DataBarFormComponent, CompartilhadoModule, MaterialCoreModule]
+    exports: [DataBarFormComponent, DataBarModule, CompartilhadoModule, MaterialCoreModule]
 })
 export class DataBarFormModule {
 }
