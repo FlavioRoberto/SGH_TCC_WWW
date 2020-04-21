cd53b49a4c705e27edddb8aebe6a9068a31800e5
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2020-04-21 12:45:07 -0300

    Criando layout do QDA

--- www/src/app/main/gerenciamento/horarios/quadro-horario-aula/quadro-horario-aula.component.ts
+++ www/src/app/main/gerenciamento/horarios/quadro-horario-aula/quadro-horario-aula.component.ts
@@ -6,7 +6,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class QuadroHorarioAulaComponent implements OnInit {
 
+    horarios = ['07:00', '08:00', '09:00', 'Intervalo', '10:00', '11:00', '12:00'];
+
+    diasDaSemana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+
+    aulas = [
+        {
+            horario: '07:00',
+            dia: 'Segunda',
+            professor: 'Flávio'
+        },
+        {
+            horario: '09:00',
+            dia: 'Segunda',
+            professor: 'Flávio'
+        },
+    ];
+
+    retornarAulas(horario: string, diaSemana: string): any[] {
+        return this.aulas.filter(lnq => lnq.dia === diaSemana && lnq.horario === horario);
+    }
+
+    existeHorario(horario, dia): boolean {
+        return this.aulas.filter(lnq => lnq.dia === dia && lnq.horario === horario).length > 0;
+    }
+
     private _codigoHorario: number;
+
+    dataSource = [];
 
     constructor(private _route: ActivatedRoute) { }
 
--- www/src/app/main/gerenciamento/horarios/quadro-horario-aula/quadro-horario-aula.module.ts
+++ www/src/app/main/gerenciamento/horarios/quadro-horario-aula/quadro-horario-aula.module.ts
@@ -4,13 +4,17 @@ import { TranslateModule } from '@ngx-translate/core';
 import { FuseSharedModule } from '@fuse/shared.module';
 import { CompartilhadoModule } from '@compartilhado/compartilhado.module';
 import { QuadroHorarioAulasRoutingModule } from './quadro-horario-aula.routes.module';
+import { MaterialCoreModule } from 'app/core/modules/material-core.module';
+import { QuadroHorarioAulaComponent } from './quadro-horario-aula.component';
 
 
 @NgModule({
+    declarations: [QuadroHorarioAulaComponent],
     imports: [
         CompartilhadoModule,
         TranslateModule,
         FuseSharedModule,
+        MaterialCoreModule,
         QuadroHorarioAulasRoutingModule
     ]
 })
