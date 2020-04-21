e8d91237daaa71b1ca4b79cdbe1094e97dc53ba0
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2020-04-21 14:41:17 -0300

    Adicionando efeito de hover

--- www/src/app/main/gerenciamento/horarios/quadro-horario-aula/services/quadro-horario-aula.service.ts
+++ www/src/app/main/gerenciamento/horarios/quadro-horario-aula/services/quadro-horario-aula.service.ts
@@ -4,11 +4,11 @@ import { QuadroHorarioModel } from '../model/quadro-horario-model';
 @Injectable()
 export class QuadroHorarioAulaService {
     listarHorarios(): string[] {
-        return ['07:00', '08:00', '09:00', '10:00', '11:00', '12:00'];
+        return ['07:00', '07:50', '08:40', '9:45', '10:35', '11:25'];
     }
 
     listarDiasSemana(): string[] {
-        return ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+        return ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
     }
 
     listarAulas(): QuadroHorarioModel[] {
@@ -21,27 +21,27 @@ export class QuadroHorarioAulaService {
                 professor: 'Flávio Roberto Teixeira',
                 sala: '311',
                 dia: 'Segunda',
-                horario: '07:00'
+                horario: '07:50'
             },
             {
                 codigo: 1,
                 codigoCargo: 1,
                 codigoSala: 1,
                 disciplina: 'Engenharia de software',
-                professor: 'Flávio Roberto Teixeira',
-                sala: '311',
-                dia: 'Terça',
-                horario: '07:00'
-            },
-            {
-                codigo: 1,
-                codigoCargo: 1,
-                codigoSala: 1,
-                disciplina: 'Engenharia de software',
-                professor: 'Flávio Roberto Teixeira',
+                professor: 'Cargo 42',
                 sala: '311',
                 dia: 'Quarta',
-                horario: '07:00'
+                horario: '09:45'
+            },
+            {
+                codigo: 1,
+                codigoCargo: 1,
+                codigoSala: 1,
+                disciplina: 'Arquitetura de computadores',
+                professor: 'Jhonatan Fernando De Oliveira',
+                sala: '311',
+                dia: 'Quinta',
+                horario: '11:25'
             },
             {
                 codigo: 1,
@@ -51,16 +51,16 @@ export class QuadroHorarioAulaService {
                 professor: 'Flávio Roberto Teixeira',
                 sala: '311',
                 dia: 'Quinta',
-                horario: '07:00'
+                horario: '08:40'
             },
             {
                 codigo: 1,
                 codigoCargo: 1,
                 codigoSala: 1,
-                disciplina: 'Engenharia de software',
-                professor: 'Flávio Roberto Teixeira',
+                disciplina: 'Sistemas distribuidos',
+                professor: 'Cristina Maria Valadares',
                 sala: '311',
-                dia: 'Sexta',
+                dia: 'Segunda',
                 horario: '07:00'
             },
             {
@@ -71,7 +71,17 @@ export class QuadroHorarioAulaService {
                 professor: 'Flávio Roberto Teixeira',
                 sala: '311',
                 dia: 'Sábado',
-                horario: '07:00'
+                horario: '09:45'
+            },
+            {
+                codigo: 1,
+                codigoCargo: 1,
+                codigoSala: 1,
+                disciplina: 'Arquitetura de computadores',
+                professor: 'Elton Ângelo Rabelo',
+                sala: '311',
+                dia: 'Terça',
+                horario: '10:35'
             }
         ] as QuadroHorarioModel[];
     }
