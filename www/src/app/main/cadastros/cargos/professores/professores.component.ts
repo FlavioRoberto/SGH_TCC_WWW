68b93c5978aa2d9ed3cacf5efe0dc2b66df8f4ba
Flávio <flaviolive3@hotmail.com>
2021-06-30 20:53:04 -0300

    Aumento quantidade de caracteres do campo matrícula do professor

--- www/src/app/main/cadastros/cargos/professores/professores.component.ts
+++ www/src/app/main/cadastros/cargos/professores/professores.component.ts
@@ -50,7 +50,7 @@ export class ProfessoresComponent implements IDataBarBindComponent<Professor> {
             nome: new FormControl(null, [Validators.required, Validators.maxLength(45)]),
             telefone: new FormControl(null, [Validators.maxLength(20), Validators.pattern(celularRegex)]),
             email: new FormControl(null, [Validators.maxLength(45), Validators.required, Validators.email]),
-            matricula: new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.pattern(matriculaRegex)]),
+            matricula: new FormControl(null, [Validators.required, Validators.maxLength(11), Validators.pattern(matriculaRegex)]),
             ativo: new FormControl(null)
         });
     }
